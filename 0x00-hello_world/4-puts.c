7561b3a501ceacdaf65752e345b68f5bd595b5a3
Pizzosta <pizzostor@gmail.com>
2022-10-15 06:14:57 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: Returns 0
+ * main - prints text
+ *
+ * Return: 0 if succesful
  */
 
 int main(void)
 
 {
 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
