f1a9951c42fbb7f4631b9d684a46e5cf84a7529e
Pizzosta <pizzostor@gmail.com>
2022-10-19 19:36:38 -0700

    a function that prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char u, i;
+
+	for (u = 0, u <= 9, u++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar("\n");
+	}
+
+}
