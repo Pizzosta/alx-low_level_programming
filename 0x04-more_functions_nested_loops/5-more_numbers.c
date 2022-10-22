f2082f8e638596af0663e1f5909624c8c899826b
Pizzosta <pizzostor@gmail.com>
2022-10-22 15:01:56 -0700

    prints 0-14 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * more_numbers - fucn prints 0-14
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
