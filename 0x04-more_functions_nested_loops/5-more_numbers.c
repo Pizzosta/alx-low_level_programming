80f09735ad33d98c6c75af37784c91d5ce630fb8
Pizzosta <pizzostor@gmail.com>
2022-10-22 15:11:48 -0700

    prints 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,20 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	a = 0;
+
+	while (a < 10)
 	{
 		for (b = 0; b < 15; b++)
 		{
 			if (b >= 10)
+			{
 				_putchar((b / 10) + 48);
+			}
 			_putchar((b % 10) + 48);
 		}
 		_putchar('\n');
-	
+
 		a++;
 	}
 }
