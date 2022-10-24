7ab7734d3f670aca9ceb27fdef39ca9214183edf
Pizzosta <pizzostor@gmail.com>
2022-10-23 17:11:12 -0700

    FizzBuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,21 @@ int main(void)
 {
 	int i;
 
+	i = 1;
+	printf("d%", n);
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ")
+			printf(" FizzBuzz")
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
