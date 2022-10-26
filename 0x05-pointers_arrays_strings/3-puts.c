943418a03ba98425d588df99a42b02f80d9d7e30
Pizzosta <pizzostor@gmail.com>
2022-10-26 16:13:31 -0700

    prints a string on a newline to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by a newline
+ * @str: input string
+ * Return: always 0 succesful
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+
+	return (0);
+}
