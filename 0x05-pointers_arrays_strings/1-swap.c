1b63ce9aaa4cb2e2ffa52b7110ce2d17a9a44597
Pizzosta <pizzostor@gmail.com>
2022-10-26 11:15:41 -0700

    swap 2 integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: always 0 successful
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
