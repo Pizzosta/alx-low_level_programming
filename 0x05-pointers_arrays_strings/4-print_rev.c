f9c8d479c80f22be93505695eeb80b097fbb8ac8
Pizzosta <pizzostor@gmail.com>
2022-10-26 16:59:58 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: input string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	rev(s);
+	_putchar('\n');
+}
