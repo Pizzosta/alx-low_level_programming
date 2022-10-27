9fac8357ba83451e6f3890084bbae3ee5876ab19
Pizzosta <pizzostor@gmail.com>
2022-10-26 19:00:24 -0700

    a reverse function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,18 +2,20 @@
 #include <string.h>
 
 /**
- * rev_string - prints a string in reverse
- * @s: input string
- * Return: no return
- */
+* rev_string - prints a string in reverse
+* @s: input string
+* Return: no return
+*/
 
 void rev_string(char *s)
 {
-	int leng, g;
+int i, len, temp;
 
-	leng = strlen(s);
-	for (g = leng - 1; g >= 0; g--)
-	{
-		s[g];
-	}
+len = strlen(s);
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+}
 }
