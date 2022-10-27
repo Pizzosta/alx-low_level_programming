b5f9c89dd9a44837a24a70f366d332e7a7a0327b
Pizzosta <pizzostor@gmail.com>
2022-10-27 15:29:09 -0700

    print every character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - function a function that prints every other character
+ * of a string, starting with the first character
+ *
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; c <= strlen(str) - 1; c++)
+	{
+		if (c % 2 == 0)
+			_putchar(str[c]);
+	}
+	_putchar('\n');
+}
