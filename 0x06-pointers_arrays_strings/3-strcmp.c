d0c071c50b09e3091b02005d70e8b20cc1bda287
Pizzosta <pizzostor@gmail.com>
2022-10-28 18:57:51 -0700

    a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcmp - a function that concatenates two strings.
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+
+char *_strcmp(char *dest, char *src)
+{
+	strcmp(dest, src);
+
+	return (dest);
+}
