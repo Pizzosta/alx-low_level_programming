a8b07955c10c4dfbb3d8c2a01a044c2d65c7bd90
Pizzosta <pizzostor@gmail.com>
2022-10-28 18:10:48 -0700

    concates 2 strings using strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - a fucntion thats copies a string
+ * @dest: string 1
+ * @src: string 2
+ * @n: param
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	dest[strlen(dest) + i] = src[i];
+	dest[strlen(dest) + i] = '\0';
+	return (dest);
+}
