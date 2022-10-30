2944486ae11bd8e3c564041c08bc7482aa0f7788
Pizzosta <pizzostor@gmail.com>
2022-10-29 17:40:41 -0700

    compares 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,16 @@
 char *_strcmp(char *s1, char *s2)
 {
 	int flag = 0;
+	int i = 0;
 
 	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-			flag = 1
+			flag = 1;
+			break;
 		}
-		s1++;
-		s2++;
+		i++;
 	}
 	if (flag == 0)
 		return (0);
