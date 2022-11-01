f4a073a9616b9120ad9a80798840edc2ae11e8aa
Pizzosta <pizzostor@gmail.com>
2022-11-01 09:21:02 -0700

    create my own memset function in c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,13 @@
 char *_memset(char *s, char b, unsigned int n);
 {
 	int i;
+	unsigned char *x = s;
 	i = 0;
 
 	while (n > 0)
 	{
-		*s = b;
-		s++;
+		*x = (unsigned char) b;
+		x++;
 		n--;
 	}
 
