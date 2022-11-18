29e30d86f94c05a08cddedfa0be203e76ca7f87b
Pizzosta <pizzostor@gmail.com>
2022-11-18 03:34:06 -0800

    i

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,10 +7,11 @@
  * Return: Always 0 success
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *operator;
+	char operator;
+	int (*calc)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,22 +20,25 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	operator = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	calc = get_op_func(argv[2]);
+
+	if (!calc)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*operator == '/' || operator == "%") && num2 == 0)
+	operator = *argv[2];
+
+	if ((operator == '/' || operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(operator)(num1, num2));
+	printf("%d\n", calc(num1, num2));
 
 	return (0);
 }
