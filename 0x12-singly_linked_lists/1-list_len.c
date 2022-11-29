6ca19fd1ca144e2f1d8e82fae5fe78454608f398
Pizzosta <pizzostor@gmail.com>
2022-11-29 07:50:20 -0800

    returns the number of elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodelem;
+
+	nodelem = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nodelem++;
+	}
+	return (nodelem);
+}
